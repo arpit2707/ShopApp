f16d0e842096690c321f5273435486392ef15167
arpit2707 <arpitgaurav.goldi723@gmail.com>
2023-04-10 01:19:23 +0530

    ShopAppComplete

---
+++ Admin_Shop/routes/contactUs.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/contactUs', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'contactUs.html'));
+});
+
+// /admin/add-product => POST
+router.post('/success',(req, res, next) => {
+  console.log(req.body);
+  res.sendFile(path.join(rootDir,'views','success.html'));
+});
+
+module.exports = router;
