037580327a98fb1ee3c9edc878fcbdf1967060dc
arpit2707 <arpitgaurav.goldi723@gmail.com>
2023-04-10 12:09:32 +0530

    Added_Controllers

--- Admin_Shop/routes/admin.js
+++ Admin_Shop/routes/admin.js
@@ -2,19 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
 
 const router = express.Router();
-
+const adminController=require('../controllers/products');
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-});
+router.get('/add-product', adminController.getProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
+router.post('/add-product', adminController.postAddProducts);
 
 module.exports = router;
